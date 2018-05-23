d0dc5e4ddb347d50463fcc034c4a726bcde326c7
Pierre Leresteux <pierre.leresteux@gmail.com>
2018-05-23 23:08:19 +0200

    Replace jade with hogan

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var timerMiddleware = require('../middleware/timer');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CodeursEnSeine Timer' });
+  res.render('index', { title: 'CodeursEnSeine Timers', timers: timerMiddleware.getAllTimers() });
 });
 
 module.exports = router;
