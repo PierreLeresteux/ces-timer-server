d4f8c49e886db902fd3e62b19ad4049b50e45eeb
Pierre Leresteux <pierre.leresteux@gmail.com>
2018-05-24 13:44:43 +0200

    Add check and blacklist

--- middleware/timer.js
+++ middleware/timer.js
@@ -1,4 +1,8 @@
+const request = require('request');
+
 var id=0;
+const delayCalltimer=3000;
+const blacklistError=3;
 const timer = {
     
     timers:[],
@@ -20,10 +24,31 @@ const timer = {
     },
 
     setRoom(id,room){
+        id=Number(id);
         var timer = this.getTimer(id);
         timer.room=room;
-        this.timers[this.timers.findIndex(function(e){return e.id===id})]=timer;
+        var index=this.timers.findIndex(function(e){return e.id===id})
+        this.timers[index]=timer;
+        setTimeout(()=>{this.callTimer(index)},delayCalltimer);
         return timer;
+    },
+    callTimer(index){
+        var timer=this.timers[index];
+        console.log(timer.room)
+        request('http://'+timer.ip+":3001/ruok", { json: true }, (err, res, body) => {
+            if (err || body!=='ok'){
+                timer.error+=1
+                this.timers[index]=timer
+            }
+            if (timer.error>=blacklistError){
+                console.log("Remove "+timer.room)
+                this.timers.splice(index, 1);
+            }else{
+                console.log(JSON.stringify(body));
+                setTimeout(()=>{this.callTimer(index)},delayCalltimer);
+            }
+        });  
+        
     },
     nextId(){   
         id+=1;
@@ -34,6 +59,7 @@ const timer = {
 var Timer = function(id,ip){
     this.id=id;
     this.ip=ip;
+    this.error=0;
     this.room=null;
 }
 
